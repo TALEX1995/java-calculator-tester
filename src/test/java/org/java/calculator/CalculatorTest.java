c004701f10d9d2437a016bee9d4348b06161cd87
TALEX1995 <tanase.alex@libero.it>
2023-11-27 15:20:41 +0100

    minorfix Calculator Test

--- src/test/java/org/java/calculator/CalculatorTest.java
+++ src/test/java/org/java/calculator/CalculatorTest.java
@@ -19,29 +19,29 @@ public class CalculatorTest {
 	@Test
 	public void checkAdd() {
 		float result = calc.add(4, 5);
-		
-		assertEquals(9, result, "I due numeri dell'addizione non sono uguali");
+		float expectedResult = 4 + 5;
+		assertEquals(expectedResult, result, "I due numeri dell'addizione non sono uguali");
 	}
 	
 	@Test
 	public void checkSubstract() {
 		float result = calc.substract(10, 5);
-		
-		assertEquals(5, result, "I due numeri della sottrazione non sono uguali");
+		float expectedResult = 10 - 5;
+		assertEquals(expectedResult, result, "I due numeri della sottrazione non sono uguali");
 	}
 	
 	@Test
 	public void checkDivide() throws Exception {
 		float result = calc.divide(5, 2);
-		
-		assertEquals(2.5, result, "I due numeri della divisione non sono uguali");
+		float expectedResult = 5.0f / 2;
+		assertEquals(expectedResult, result, "I due numeri della divisione non sono uguali");
 	}
 	
 	@Test
 	public void checkMultiply() {
 		float result = calc.multiply(3, 10);
-		
-		assertEquals(30, result, "I due numeri della divisione non sono uguali");
+		float expectedResult = 3 * 10;
+		assertEquals(expectedResult, result, "I due numeri della divisione non sono uguali");
 	}
 	
 	@Test
